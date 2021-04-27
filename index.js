c230dce0fb183c5a81ce874f7e74a7c5a2513e8e
Alex Calvert <alexanderajcalvert@gmail.com>
2021-04-26 17:32:45 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let cards = [`Ada`, `Brendan`, `Ali`];
+let typeOfCard = `birthday`;
+
+function writeCards(cards,typeOfCard) {
+    let recipients = [];
+    for (let i = 0; i < cards.length; i++) {
+        recipients.push (`Thank you, ${[cards[i]]}, for the wonderful ${typeOfCard} gift!`);
+    }
+return recipients;
+}
+console.log(writeCards(cards, typeOfCard));
+
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+        console.log(i);
+    }
+}
